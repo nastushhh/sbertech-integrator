0a25647c2e5642eafab7a907ef9f03aefe0cc284
nastushh <kolodkinanast@yandex.ru>
2025-03-14 14:21:34 +0300

    update javadoc

--- src/main/java/com/sber/phonebook/generation/randomGen.java
+++ src/main/java/com/sber/phonebook/generation/randomGen.java
@@ -3,11 +3,13 @@ package com.sber.phonebook.generation;
 import java.util.concurrent.ThreadLocalRandom;
 /**
  * Класс для генерации случайных чисел в заданных диапазонах.
+ * Использует {@link ThreadLocalRandom} для генерации случайных значений.
+ * <p>Содержит методы для получения случайных чисел N, M, T в определенных диапазонах</p>
  */
 public class randomGen{
     /**
      *Генерирует случайное значение N в диапазоне от 500 до 5000.
-     * @return случайное число N
+     * @return случайное число N: 500 < N < 5000.
      */
     public static int getRandomN(){
         return ThreadLocalRandom.current().nextInt(501, 5000);
@@ -15,7 +17,7 @@ public class randomGen{
 
     /**
      *Генерирует случайное значение M в диапазоне от 0 до 30
-     * @return случайное число M
+     * @return случайное число M: 0 < M < 30.
      */
     public static int getRandomM(){
         return ThreadLocalRandom.current().nextInt(0, 30);
@@ -23,7 +25,7 @@ public class randomGen{
 
     /**
      * Генерирует случайное значение T (60 < T < 300)
-     * @return случайное число T
+     * @return случайное число T: 60 < T < 300.
      */
     public static int getRandomT(){
         return ThreadLocalRandom.current().nextInt(61, 300);
